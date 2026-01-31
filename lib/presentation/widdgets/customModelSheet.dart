4d206c65cc081e974c34e99f751ddda2494e3c05
rabh777 <prorabh777@gmail.com>
2026-01-31 21:30:27 +0200

    Add AbsorbPointer at Loading

--- lib/presentation/widdgets/customModelSheet.dart
+++ lib/presentation/widdgets/customModelSheet.dart
@@ -3,7 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:lucide_icons/lucide_icons.dart';
 import 'package:noteapp/business_logic/add_note_cubit/cubit/add_note_cubit_cubit.dart';
 import 'package:noteapp/presentation/widdgets/customBottom.dart';
-import 'package:noteapp/presentation/widdgets/customFormFiled.dart';
+import 'package:noteapp/presentation/widdgets/customNoteForm.dart';
 import 'package:noteapp/presentation/widdgets/customSnakBar.dart';
 import 'package:noteapp/presentation/widdgets/customSpinKit.dart';
 import 'package:noteapp/presentation/widdgets/cutomTextFeild.dart';
@@ -13,39 +13,43 @@ class CustomModelSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var singleChildScrollView = SingleChildScrollView(
-      child: BlocConsumer<AddNoteCubit, AddNoteState>(
-        listener: (context, state) {
-          if (state is AddNoteSuccess) {
-            Navigator.pop(context);
-            showSnakBar(
-              context: context,
-              message: 'Note Added Successfully',
-              backgroundColor: Colors.green[700],
-              icons: LucideIcons.checkCircle,
+    return BlocProvider(
+      create: (context) => AddNoteCubit(),
+      child: SingleChildScrollView(
+        child: BlocConsumer<AddNoteCubit, AddNoteState>(
+          listener: (context, state) {
+            if (state is AddNoteSuccess) {
+              Navigator.pop(context);
+              showSnakBar(
+                context: context,
+                message: 'Note Added Successfully',
+                backgroundColor: Colors.green[700],
+                icons: LucideIcons.checkCircle,
+              );
+            }
+            if (state is AddNoteFailure) {
+              Navigator.pop(context);
+              showSnakBar(
+                context: context,
+                message: 'Falied ${state.errMessage}',
+                backgroundColor: Colors.red[700],
+                icons: LucideIcons.xCircle,
+              );
+            }
+          },
+          builder: (context, state) {
+            if (state is AddNoteLoading) {
+              WidgetsBinding.instance.addPostFrameCallback((_) {
+                customShowDialog(context);
+              });
+            }
+            return AbsorbPointer(
+              absorbing: state is AddNoteLoading ? true : false,
+              child: CustomNoteForm(),
             );
-          }
-          if (state is AddNoteFailure) {
-            Navigator.pop(context);
-            showSnakBar(
-              context: context,
-              message: 'Falied ${state.errMessage}',
-              backgroundColor: Colors.red[700],
-              icons: LucideIcons.xCircle,
-            );
-          }
-        },
-        builder: (context, state) {
-          if (state is AddNoteLoading) {
-            WidgetsBinding.instance.addPostFrameCallback((_) {
-              customShowDialog(context);
-            });
-          }
-
-          return CustomNoteForm();
-        },
+          },
+        ),
       ),
     );
-    return singleChildScrollView;
   }
 }
--- lib/presentation/widdgets/customNoteForm.dart
+++ lib/presentation/widdgets/customNoteForm.dart
@@ -17,63 +17,50 @@ class _CustomNoteFormState extends State<CustomNoteForm> {
   AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
   String? title, content;
   @override
-
   Widget build(BuildContext context) {
     return Form(
       key: formKey,
       autovalidateMode: autovalidateMode,
       child: Column(
         children: [
-          /// The main content of the form.
           Container(
-            height: 370,
+            height: 330,
             child: ListView(
               children: [
-                /// A space of 14 units.
                 SizedBox(height: 14),
 
-                /// The first text field with a title hint.
                 Padding(
                   padding: const EdgeInsets.only(left: 15, right: 15, top: 14),
                   child: CustomTextField(
-                    /// When the user saves the text, set the title to the saved value.
                     onSaved: (val) {
                       title = val;
                     },
-                    /// The title hint of the text field.
                     hintText: 'title',
-                    /// The label of the text field.
                     lable: 'title',
                   ),
                 ),
-
-                /// A space of 12 units.
                 SizedBox(height: 12),
-
-                /// The second text field with a content hint.
                 Padding(
                   padding: const EdgeInsets.only(left: 15, right: 15, top: 14),
                   child: CustomTextField(
-                    /// When the user saves the text, set the content to the saved value.
                     onSaved: (val) {
                       content = val;
                     },
-                    /// The content hint of the text field.
                     hintText: 'content',
-                    /// The label of the text field.
                     lable: 'Content',
                   ),
                 ),
 
-                /// A custom button with the label 'Add'.
                 CustomButton(
                   text: 'Add',
-                  /// When the button is pressed, validate the form and if valid, save
-                  /// the form and add a note to the bloc.
                   onPressed: () {
                     if (formKey.currentState!.validate()) {
                       formKey.currentState!.save();
-                      var note= NoteModel(title: title!, content: content!, date: DateTime.now().toString());
+                      var note = NoteModel(
+                        title: title!,
+                        content: content!,
+                        date: DateTime.now().toString(),
+                      );
                       BlocProvider.of<AddNoteCubit>(context).addNote(note);
                     } else {
                       setState(() {
@@ -89,5 +76,4 @@ class _CustomNoteFormState extends State<CustomNoteForm> {
       ),
     );
   }
-
 }
